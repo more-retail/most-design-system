05dce55e6793eb794c6a4aeaa8853dd7a84a6e91
Ankit Dewangan <ankit.dewangan@moreretail.in>
2026-04-20 17:00:02 +0530

    feat(input-group): add react native InputGroup components

---
+++ src/components/InputGroup/index.native.ts
@@ -0,0 +1,15 @@
+export {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupText,
+  InputGroupInput,
+  InputGroupTextarea,
+} from "./InputGroup.native";
+export type {
+  InputGroupProps,
+  InputGroupAddonProps,
+  InputGroupAddonAlign,
+  InputGroupInputProps,
+  InputGroupTextareaProps,
+} from "./InputGroup.native";
