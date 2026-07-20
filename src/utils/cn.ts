6fa52838f11899400f9c4f89bd04564954f14388
IKA3RUS <w@abhishekrein.xyz>
2026-07-20 16:21:25 +0530

    chore: update tailwind merge config

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,5 +1,42 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const widthSuffixes = [
+  "base-10",
+  "base-20",
+  "base-30",
+  "semantic-default",
+  "semantic-thick",
+  "semantic-focus",
+];
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      rounded: [
+        {
+          rounded: [
+            "base-0",
+            "base-10",
+            "base-20",
+            "base-30",
+            "base-40",
+            "base-50",
+            "base-60",
+            "base-70",
+            "base-80",
+            "base-90",
+            "base-100",
+            "base-full",
+          ],
+        },
+      ],
+      "border-w": [{ border: widthSuffixes }],
+      "ring-w": [{ ring: widthSuffixes }],
+      "outline-w": [{ outline: widthSuffixes }],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -11,4 +48,8 @@ export interface Cn {
 
 export interface OptionalCn {
   cn?: typeof cn;
+}
+
+export function tw(strings: TemplateStringsArray) {
+  return strings[0];
 }
